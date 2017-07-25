c0fcb1a7fa113cac4ef37b7eb851ec21bb343c9c
Edwin Diaz <=>
2017-07-25 17:02:33 -0400

    implemented model factories

--- database/migrations/2016_04_23_201840_create_posts_table.php
+++ database/migrations/2016_04_23_201840_create_posts_table.php
@@ -12,14 +12,14 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->unsigned()->index();
-            $table->integer('category_id')->unsigned()->index();
-            $table->integer('photo_id')->unsigned()->index();
-            $table->string('title');
-            $table->text('body');
-            $table->timestamps();
+            Schema::create('posts', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id')->unsigned()->index();
+                $table->integer('category_id')->unsigned()->index();
+                $table->integer('photo_id')->unsigned()->index();
+                $table->string('title');
+                $table->text('body');
+                $table->timestamps();
 
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
